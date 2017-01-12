77d1594b269863c10a4b6a68f6246e67887d9854
zeluliCredit <zeluli@creditease.cn>
2017-01-12 17:34:19 +0800

    project init

---
+++ CEThemeSwitcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CEThemeSwitcher
+//
+//  Created by Mr.LuDashi on 2017/1/12.
+//  Copyright © 2017年 ZeluLi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
